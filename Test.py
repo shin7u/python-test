4bece5a31cca3724eab2b4b4ecd27a89d816264a
Achmad Agam <64517057+shin7u@users.noreply.github.com>
2020-10-23 20:36:16 +0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+nilai = 9
+
+if(nilai > 7):
+    print('Sip percobaan Sukses')
+else:
+    print('Percobaan sukses tapi nilai kurang memuaskan')
+
+count = 0
+while(count < 9 ):
+    print('Hitungan', +count)
+    count +=1
+print('done')
